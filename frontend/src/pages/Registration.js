9acabb60feee5cc3617e4f5df5db4a0c4223cb2f
Chia-Ying Chao <anniebi1003@gmail.com>
2023-11-20 12:19:00 -0500

    use this folder

---
+++ frontend/src/pages/Registration.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Registration = () =>{
+    return(
+        <div style={{
+            display: "flex",
+            justifyContent: 'center',
+            height:'',
+        }}>
+
+
+        </div>
+
+    )
+};
+export default Registration;
