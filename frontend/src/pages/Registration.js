359e3c9d46cdd19c9cf4910dca0e5426ab53181e
Chia-Ying Chao <anniebi1003@gmail.com>
2023-11-20 12:55:21 -0500

    Update Registration.js

--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -2,11 +2,7 @@ import React from "react"
 
 const Registration = () =>{
     return(
-        <div style={{
-            display: "flex",
-            justifyContent: 'center',
-            height:'',
-        }}>
+        <div style={{display: "flex",justifyContent: 'center',height:'100vh',backgroundColor: '#1B2432', }}>
 
 
         </div>
