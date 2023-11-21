4652dfaf697ddbdf12137cce0138acdd350d572c
Chia-Ying Chao <anniebi1003@gmail.com>
2023-11-21 16:15:35 -0500

    add input

--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Input, TextInput } from '@mantine/core';
 
 const Registration = () =>{
+    // Incorrect usage, input is not accessible
+    function Incorrect() {
+        return (
+        <Input.Wrapper label="Input label">
+            <Input />
+        </Input.Wrapper>
+        );
+    }
+    
+    // Use TextInput instead of Input everywhere you want to use Input,
+    // it is accessible by default and includes Input.Wrapper
+    function Correct() {
+        return <TextInput label="Input label" description="Input description" />;
+    }
+  
+
     return(
         <div className="registration">
-            
+            <Input placeholder="Input component" />
         </div>
 
     )
