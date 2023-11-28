beb58f81a305d09e98a6f15abe4a1723880e244d
Chia-Ying Chao <anniebi1003@gmail.com>
2023-11-28 15:16:06 -0500

    added

--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -20,6 +20,29 @@ const Registration = () =>{
         [name]:value 
 
     }));
+    //clear errors for the field when the user starts typing
+    if(errors[name]){
+        setErrors((prevErrors)=>({
+            ...prevErrors,
+            [name]: ''
+
+        }));
+
+    }
+
+   };
+
+   const handleSubmit = (event) => {
+    event.preventDefault();
+    //Array list to store the error messages 
+    let errorMessages = [];
+    //Check for empty fields, if there is one, add it to the errorMessages list
+    Object.keys(data).forEach((key)=>{
+        if(!data[key].trim()){
+            const field = key.charAt(0).toUpperCase()+ key.slice(1).replace(/([A-Z])/g, ' $1').trim();
+            errorMessages.push(`{field} is required!`);
+        }
+    });
 
    }
 
