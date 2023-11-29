9599da537c4af104c612641110235afdb03db49b
Chia-Ying Chao <anniebi1003@gmail.com>
2023-11-28 23:06:25 -0500

    start sign in page

--- frontend/src/pages/signIn.js
+++ frontend/src/pages/signIn.js
@@ -3,11 +3,17 @@ import React from "react";
 
 const SignIn = () =>{
     return (
-        <div style={{display: "flex",justifyContent: 'center',height:'100vh',backgroundColor: '#1B2432', }}>
+        <div className="signIn" >
+            <div className="circle1"></div>
+            <div className="circle2"></div>
+            <div className="circle3"></div>
+            <div className="circle4"></div>
+            <div className="circle5"></div>
+           
 
 
         </div>
-    )
+    );
    
 
 
