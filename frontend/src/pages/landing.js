33faf87b0ad39aa4596276be6c4a9c8d2cc7dedf
Chia-Ying Chao <anniebi1003@gmail.com>
2023-11-30 11:13:15 -0500

    starting landing page

--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -4,14 +4,39 @@ import tree from "../components/tree";
 
 
 const Landing = () =>{
+    const headline1 = "Beyond words, Above Expectations -"
+    const headline2 = "Your Nest for Exceptional Communication!"
     return (
         <div >
             <Navbar/>
             
             <div className="landing">
                 <div className="row">
-                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;Beyond words, Above Expectations - <br /> Your Nest for Exceptional Communication! </h2>
-                    <buttons></buttons>
+                    <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        {headline1.split('').map((letter, index)=> 
+                        letter === " " ? (
+                            <>&nbsp;</>) :(
+                            <span key = {index} className="bounce">{letter}</span>
+                        ))}
+                        <br />
+                        
+                        {headline2.split('').map((letter, index)=> 
+                        letter === " " ? (
+                            <>&nbsp;</>) :(
+                            <span key = {index} className="bounce">{letter}</span>
+                        ))}
+                    </h2>
+
+                    <div className="buttons">
+                        <button>Download to your Desktop</button>
+                        <button>Open Perch in your browser</button>
+
+
+                    </div>
+
+                    
+                    
+                    
 
                 </div>
                 
