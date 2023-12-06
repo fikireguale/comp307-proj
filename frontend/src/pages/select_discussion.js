92f40a59a562bcff4a077ebf4062d0558217b596
Yuka <89941318+Katie-cube@users.noreply.github.com>
2023-12-06 12:42:48 -0500

    Update select_discussion.js

--- frontend/src/pages/select_discussion.js
+++ frontend/src/pages/select_discussion.js
@@ -13,6 +13,44 @@ const Select_Discussion =() =>{
                 <button class="logout" onclick={redirectToLanding}>Logout</button>
             </header>
 
+            <div class="upper_bar">
+                <h1>Courses</h1>
+                <button class="create_board">+</button>
+                <a>Course Name</a>
+            </div>
+            
+            <div class="select_board">
+                <div class="item">
+                    <img src="../assets/course_img.jpeg"></img>
+                    <div class="course_text">
+                        <h2>Course name1</h2>
+                        <p>course description</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/course_img.jpeg"></img>
+                    <div class="course_text">
+                        <h2>Course name2</h2>
+                        <p>course description</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/course_img.jpeg"></img>
+                    <div class="course_text">
+                        <h2>Course name3</h2>
+                        <p>course description</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="../assets/course_img.jpeg"></img>
+                    <div class="course_text">
+                        <h2>Course name4</h2>
+                        <p>course description</p>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     )
 };
