02d833c9812b8dd8270083eef9bffc7f8a2784a1
Chia-Ying Chao <anniebi1003@gmail.com>
2023-12-08 19:34:54 -0500

    deleted tree and moved circles into sigin div

--- frontend/src/pages/signIn.js
+++ frontend/src/pages/signIn.js
@@ -77,13 +77,14 @@ const SignIn = () =>{
 
     return (
         <div className="signIn" >
-            <div className="circle1"></div>
-            <div className="circle2"></div>
-            <div className="circle3"></div>
-            <div className="circle4"></div>
-            <div className="circle5"></div>
             
             <form className="login-form" onSubmit={handleSubmit}>
+                <div className="circle1"></div>
+                <div className="circle2"></div>
+                <div className="circle3"></div>
+                <div className="circle4"></div>
+                <div className="circle5"></div>
+            
                 <h3>Please enter your username and password</h3>
                 <div className="row">
                     <input size='25' type ='text' name='username' placeholder="Username" onChange={handleChange}/>
