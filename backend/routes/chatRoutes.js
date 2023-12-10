df5b359a1b9e307472dde90f49b93b024fae2797
fikireguale <fikireguale@gmail.com>
2023-12-10 17:19:47 -0500

    delete chat added to backend

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -13,7 +13,7 @@ router.post('/create_chat', async (req, res) => {
         const adminUser = await User.findOne({ username: adminName });
 
         const savedChat = await Chat.create({ name: chatName, admin: adminUser._id });
-
+        // add chat to admin's userChats
         const updatedUser = await User.findByIdAndUpdate(
             adminUser._id,
             { $push: { userChats: savedChat._id } },
@@ -25,6 +25,12 @@ router.post('/create_chat', async (req, res) => {
         } else {
             console.log('No user found with the given ID');
         }
+        // add admin to Chat's users
+        await Chat.findByIdAndUpdate(
+            savedChat._id,
+            { $push: { users: adminUser._id } },
+            { new: true }
+        );
 
         res.status(201).json(savedChat);  
         
@@ -33,5 +39,47 @@ router.post('/create_chat', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 })
+
+router.post('/delete_chat', async (req, res) => {
+    try{
+        const { chatName, username } = req.body;
+
+        const user = await User.findOne({ username: username });
+
+        const chatToDelete = await Chat.findOne({ name: chatName });
+        // Check if this user is the chat admin
+        if (chatToDelete.admin.toString() !== user._id.toString()) {
+            const updatedUser = await User.findByIdAndUpdate(
+                user._id,
+                { $pull: { userChats: chatToDelete._id } },
+                { new: true }
+            );
+            
+            if (updatedUser) {
+                console.log('User updated:', updatedUser);
+            } else {
+                console.log('No user found with the given ID');
+            }
+            
+        } else {
+            // Remove the chat from all users' userChats arrays
+            await User.updateMany(
+                { _id: { $in: chatToDelete.users } }, // Assuming 'users' field contains the IDs of all users in the chat
+                { $pull: { userChats: chatToDelete._id } }
+            );
+
+            // Delete the chat for everyone
+            const deleted = await Chat.findByIdAndDelete(chatToDelete._id);
+        }
+
+        res.status(201).json(chatToDelete);  
+        
+    } catch (error) {
+        console.error('Error deleting chat', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+
 
 module.exports = router
